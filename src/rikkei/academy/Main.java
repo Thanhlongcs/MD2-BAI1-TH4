57553ee0bc7191f1f9271273785c71b12bd61ece
Thanhlongcs <thanhlongcf010@gmail.com>
2022-07-24 20:41:02 +0700

    up

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,30 @@
+package rikkei.academy;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Given a equation ax + b = c");
+
+        System.out.println("Please enter a");
+        float a = scanner.nextFloat();
+        System.out.println("Please enter b");
+        float b = scanner.nextFloat();
+        System.out.println("Please enter c");
+        float c = scanner.nextFloat();
+
+        if (a != 0){
+            float x = (c-b)/a;
+            System.out.println("x = " +x);
+        } else {
+            if(b == c) {
+                System.out.println("x is all ");
+            } else{
+                System.out.println("No solution");
+            }
+        }
+    }
+}
